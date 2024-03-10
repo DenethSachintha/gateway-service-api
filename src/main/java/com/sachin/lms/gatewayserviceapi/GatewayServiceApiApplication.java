2e464dc1fbc6476728272e0ad0dec50c70ac7912
Sachin <kbdsjayathilaka@gmail.com>
2024-03-11 00:38:56 +0530

    Initial commit

---
+++ src/main/java/com/sachin/lms/gatewayserviceapi/GatewayServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.sachin.lms.gatewayserviceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApiApplication.class, args);
+	}
+
+}
